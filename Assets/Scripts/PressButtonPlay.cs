9be4d0800fe68cddb672d1b0156f7aeeaa188030
Adelén Johansen <adelenjohansen1998@gmail.com>
2020-06-08 23:00:13 +0200

    Upload entire project to git

---
+++ Assets/Scripts/PressButtonPlay.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButtonPlay : MonoBehaviour
+{
+    public AudioSource myAudio;
+    public bool startedPlaying;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        startedPlaying = false;
+        isPaused = false;
+    }
+
+    public void RecordPlayer()
+    {
+
+        if (!startedPlaying)
+        {
+            myAudio.Play();
+            startedPlaying = true;
+        }
+        else
+        {
+            if (!isPaused)
+            {
+                myAudio.Pause();
+                isPaused = true;
+            }
+            else
+            {
+                myAudio.UnPause();
+                isPaused = false;
+            }
+        }
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
